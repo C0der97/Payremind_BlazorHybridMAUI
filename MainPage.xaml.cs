23b03d50e803f9a9c731c1b0dd810f8889b7852d
c0der97 <blae1997@gmail.com>
2024-07-28 21:47:58 -0500

    refactors and improvments

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,9 +2,34 @@
 {
     public partial class MainPage : TabbedPage
     {
+
+        private DateTime _selectedDateTime;
         public MainPage()
         {
             InitializeComponent();
+            myDatePicker.MinimumDate = new DateTime(2000, 1, 1);
+            myDatePicker.MaximumDate = DateTime.Today;
+        }
+
+        private void OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+            // Aquí puedes manejar el cambio de fecha
+            DateTime selectedDate = e.NewDate;
+            // Haz algo con la fecha seleccionada
+        }
+
+        private void OnTimeChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Time")
+            {
+                _selectedDateTime = _selectedDateTime.Date + timePicker.Time;
+                UpdateDateTimeLabel();
+            }
+        }
+
+        private void UpdateDateTimeLabel()
+        {
+            selectedDateTime.Text = $"Selected: {_selectedDateTime:g}";
         }
     }
 }
